0f71b8abf0be6f86775eeba9c6873a5de93f94fd
Ha Minh Vuong <vuong.haminh@gmail.com>
2013-07-01 13:34:29 +0700

    Add doc.

--- UIColor+Util.h
+++ UIColor+Util.h
@@ -10,16 +10,51 @@
 
 @interface UIColor (Util)
 
+///
+/// Create a random color.
+///
+/// @return A random color.
+///
 + (UIColor *)randomColor;
 
+///
+/// Create a color from hex code.
+///
+/// @param hexString A hex string. Eg. @"1A2B4F".
+/// @return A color created from hex string.
+///
 + (UIColor *)colorFromHex:(NSString *)hexString;
 
+///
+/// Create a lighter color by increase its brightness by number of percentage.
+///
+/// @param percent A percentage to increase.
+/// @return A lighter color from the orginal color.
+///
 - (UIColor *)lighterColorByPercent:(float)percent;
 
+///
+/// Create a darker color by decrease its brightness by number of percentage.
+///
+/// @param percent A percentage to decrease.
+/// @return A darker color from the original color.
+///
 - (UIColor *)darkerColorByPercent:(float)percent;
 
+///
+/// Create a lighter color by increase in the amount of its brightness.
+///
+/// @param amount An amount to increase.
+/// @return A lighter color from the original color.
+///
 - (UIColor *)lighterColorInAmount:(float)amount;
 
+///
+/// Create a darker color by decrease in the amount of its brightness.
+///
+/// @param amount An amount to decrease.
+/// @return A darker color from the originanl color.
+///
 - (UIColor *)darkerColorInAmount:(float)amount;
 
 @end
